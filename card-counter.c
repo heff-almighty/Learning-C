df9c46b7c1e5da654e1fd399a01d8647dda89d03
heff-almighty <65778069+heff-almighty@users.noreply.github.com>
2020-08-04 15:34:05 +0100

    Create card-counter.c

---
+++ card-counter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int decks;
+    puts("Enter a number of decks");
+    scanf("%i", &decks);
+    if (decks <2)
+        {
+        puts("That is not a valid number of decks");
+        return 1;
+        }
+    printf("There are %i cards\n", (decks*52));
+    return 0;
+}
