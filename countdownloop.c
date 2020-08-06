babe0b8faf55db1d99c408197d7cab44023734d9
heff-almighty <65778069+heff-almighty@users.noreply.github.com>
2020-08-06 15:09:03 +0100

    Create countdownloop.c
    Counts down in single integers from 10 to 1

---
+++ countdownloop.c
@@ -0,0 +1,15 @@
+//First loop puzzle from Headfirst book.
+//Basic loop code
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int counter;
+    for (counter = 1; counter<11; counter++)
+        printf("%i dead men swinging from the gallows\n", counter);
+
+}
